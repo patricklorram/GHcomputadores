991e3d23b1549c47447d8eb98a85fe3ac004b51c
Patrick Lorran <115165036+patricklorram@users.noreply.github.com>
2022-11-30 19:54:14 -0300

    29\11

---
+++ script.js
@@ -0,0 +1,10 @@
+function clickMenu(){
+
+    if (burger.style.display=='inline-flex'){
+        burger.style.display = 'none'
+        
+    }else{
+        burger.style.display='inline-flex'
+    }
+    
+}
